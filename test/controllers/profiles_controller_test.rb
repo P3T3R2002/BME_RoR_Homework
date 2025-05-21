8f34f325136b3a0fc4001c1c71c9d95745a34f1d
P3T3R2002 <banyai.p0227@gmail.com>
2025-05-21 21:14:16 +0200

    ALL DONE, some small changes

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,8 +1,5 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get profiles_show_url
-    assert_response :success
-  end
+  # devise
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,22 +1,5 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:nem_admin_1)
-  end
-
-  test "login" do
-    get new_user_session_path
-    assert_response :success
-  end 
-  
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
-  end
+  # devise
 end
